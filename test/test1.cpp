6b659f6664d6c3d4e5fcaa4589cbd6e040b64cd7
Takatoshi Kondo <redboltz@gmail.com>
2020-11-07 11:10:49 +0900

    Create test1.cpp

---
+++ test/test1.cpp
@@ -0,0 +1,11 @@
+#define BOOST_TEST_MODULE mqtt_client_cpp_test
+
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(tes1)
+
+BOOST_AUTO_TEST_CASE( case1 ) {
+    BOOST_TEST(true);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
