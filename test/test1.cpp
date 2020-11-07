1aed10d007ddf0f95fcc9b5428aac6ff5a93c3c6
Takatoshi Kondo <redboltz@gmail.com>
2020-11-07 15:18:16 +0900

    Update test1.cpp

--- test/test1.cpp
+++ test/test1.cpp
@@ -1,11 +1,18 @@
 #define BOOST_TEST_MODULE abp_boost_test
 
 #include <boost/test/unit_test.hpp>
+#include <string>
 
 BOOST_AUTO_TEST_SUITE(tes1)
 
 BOOST_AUTO_TEST_CASE( case1 ) {
+    int i = 42;
+    int j = 42;
     BOOST_TEST(true);
+    BOOST_TEST(i == j);
+    std::string s1 = "ABC";
+    std::string s2 = "ABC";
+    BOOST_TEST(s1 == s2);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
